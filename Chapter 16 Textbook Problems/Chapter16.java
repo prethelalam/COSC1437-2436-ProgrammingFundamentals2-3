2d55d14d4f7571a5fcee2a40327c3a04c108be12
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2024-06-05 15:56:40 -0500

    Completed textbook problem #3

--- Chapter 16 Textbook Problems/Chapter16.java
+++ Chapter 16 Textbook Problems/Chapter16.java
@@ -24,20 +24,59 @@ public class Chapter16
             // }
 
          // 2. **********************************************
-            //String array with test values
-            String[] names = {"bob", "tim", "zoey", "jim", "sam"};
+            // //String array with test values
+            // String[] names = {"bob", "tim", "zoey", "jim", "sam"};
 
-            //display array
-            System.out.println("Original Order:");
-            System.out.print(Arrays.toString(names) + " ");
+            // //display array
+            // System.out.println("Original Order:");
+            // System.out.print(Arrays.toString(names) + " ");
             
 
-            //sort the array
-            ObjectQuickSorter.quickSort(names);
+            // //sort the array
+            // ObjectQuickSorter.quickSort(names);
 
-            //display array after sorting
-            System.out.println("\nSorted Order:");
-                System.out.print(Arrays.toString(names) + " ");
-    
+            // //display array after sorting
+            // System.out.println("\nSorted Order:");
+            //     System.out.print(Arrays.toString(names) + " ");
+
+        // 3. **********************************************
+                Comparable result, searchValue;
+                String input;
+
+                //create an array of numbers
+                String number[] = {"bob", "tim", "zoey", "jim", "sam"};
+
+                Scanner scanner = new Scanner(System.in);
+
+                //sort the values
+                ObjectQuickSorter.quickSort(number);
+                
+                do
+                {
+                    //get value to search for
+                    System.out.print("Enter the value you wish to search for: ");
+                    searchValue = scanner.nextLine();
+
+                    //search for value
+                    result = ObjectBinarySearcher.binarySearch(number, searchValue);
+
+                    //display results
+                    if(result.equals(-1))
+                    {
+                        System.out.println(searchValue + " was not found");
+                    }
+                    else
+                    {
+                        System.out.println(searchValue + " was found at element " + result);
+                    }
+
+                    // //consume the remaining new line
+                    // scanner.nextLine();
+
+                    //search again option
+                    System.out.print("Do you want to search again? (Y or N): ");
+                    input = scanner.nextLine();
+                } while(input.charAt(0) == 'y' || input.charAt(0) == 'Y');
+                
     }
 }
---
+++ Chapter 16 Textbook Problems/ObjectBinarySearcher.java
@@ -0,0 +1,45 @@
+public class ObjectBinarySearcher {
+    // The IntBinarySearcher class presented in this chapter searches an int array for a specific value. 
+    // Create an ObjectBinarySearcher class that can search an array of Comparable objects. 
+    // Demonstrate the class in a program that searches for a String in an array of String objects.
+
+    public static int binarySearch(Comparable[] array, Comparable value)
+    {
+        int first;      //first array element
+        int last;       //last array element
+        int middle;     //mid point of search
+        int position;   //position of the search value
+        boolean found;  //flag 
+
+        //set intial value
+        first = 0;
+        last = array.length - 1;
+        position = -1;
+        found = false;
+
+        //search for the vlaue
+        while(!found && first <= last)
+        {
+            //calculate midpoint
+            middle = (first + last) / 2;
+
+            //value found at mid point
+            if(array[middle].compareTo(value) == 0)
+            {
+                found = true;
+                position = middle;
+            }
+            //else if value is in lower half
+            else if (array[middle].compareTo(value) > 0)
+            {
+                last = middle - 1;
+            }
+            //else if value is in upper half
+            else
+            {
+                first = middle + 1;
+            }
+        }
+        return position;
+    }
+}
